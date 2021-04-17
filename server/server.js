fd8782ecda2439388e79f047d5211b4d3ef9a7ca
bartk <bart.koszek@hotmail.com>
2021-04-17 22:57:52 +0200

    clean up server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { disconnect } = require('node:process');
 const app = express();
 const sanitizeHtml = require('sanitize-html');
 
@@ -22,59 +23,75 @@ const port = process.env.PORT || 3000;
 const debug = false;
 
 io.on('connection', client => {
-    if(debug){console.log("client connected");}
-    
-    // let idofsocket = socketgetroomname(client) // the defult room is the id of the socket client.id
-    // console.log(idofsocket);
-
-    client.on('joinroom', data => {
-        let clientrooms = getrooms(client);
-        //leave all previous rooms
-        for (let index = 1; index < clientrooms.length; index++) { //set to 1 so we dont leave socket.id room
-            const room = clientrooms[index];
-            client.leave(room);
-        }
-        client.join(data);// join new rooms
-        if(debug){console.log("client:", client.id, " joins room:", data);}
-        client.currentroom = data;
-        client.emit("joinedroom", data);// echo sucessfull data back to clinet
-        brodcasttootherplayers(client, "newjoin"); // asks clients to re-request ammount of players
+    client.on('joinroom', (data) => {
+        joinroom(client, data);
     });
-
-    client.on('setusername', data => {
-        client.username = data;
-        if(debug){console.log("client:", client.id, " is called:", client.username);}
-        client.emit("namechanged", data);
-        brodcasttootherplayers(client, "newjoin");// its as if someone new joined
+    client.on('setusername', (data) => {
+        setusername(client, data);
     });
-    
     client.on('roomclients', data => { // send back clients in current rooms
-        updateroomclients(client,data); // perhaps make it so that the client doesnt need to re send the request for players
-        // make it so server imediatly sends roomclients on newjoin
+        roomclients(client,data); // perhaps make it so that the client doesnt need to re send the request for players // make it so server imediatly sends roomclients on newjoin
     });
-
     client.on('playermove', data => {
-        console.log(data);
-        let res = [client.id, data[0], data[1], data[2]]; //id, direction, x, y
-        brodcasttootherplayers(client, "playermove", res);
+        playermove(client, data);
     });
     client.on('positionFood', data => {
-        brodcasttootherplayers(client, "positionFood");
+        positionFood(client, data);
     });
     client.on('disconnect', () => {
-        if(debug){console.log('disconnect ',client.username);}
-        // the "original" client no longer is in any rooms
-        // we replace him with a copy which is a fake to only get previous room
-        let fakeclient = new Object;
-        fakeclient.rooms = [client.id,client.currentroom]        
-        brodcasttootherplayers(fakeclient, "newjoin");
+        disconnect(client, data);
     });
 
 });
-
+function joinroom(client, data) {
+    let clientrooms = getrooms(client);
+    //leave all previous rooms
+    for (let index = 1; index < clientrooms.length; index++) { //set to 1 so we dont leave socket.id room
+        const room = clientrooms[index];
+        client.leave(room);
+    }
+    client.join(data);// join new rooms
+    if(debug){console.log("client:", client.id, " joins room:", data);}
+    client.currentroom = data;
+    client.emit("joinedroom", data);// echo sucessfull data back to clinet
+    brodcasttootherplayers(client, "newjoin"); // asks clients to re-request ammount of players
+}
+function setusername(client, data) {
+    client.username = data;
+    if(debug){console.log("client:", client.id, " is called:", client.username);}
+    client.emit("namechanged", data);
+    brodcasttootherplayers(client, "newjoin");// its as if someone new joined
+}
+function roomclients(client, data) {
+    let res = client.rooms;
+    let rooms = getrooms(client);//get the custom room, not id room
+    if(rooms.length < 2) {
+        client.emit("roomclients", []);
+        return true
+    }
+    let room = rooms[1]; //second one is the custom one joined
+    let clientidinroom = currentroomclients(room);
+    let clientnames = getsocketnames(clientidinroom); // this array includes ids
+    client.emit("roomclients", clientnames); // sends array of clients
+}
+function playermove(client, data) {
+    console.log(data);
+    let res = [client.id, data[0], data[1], data[2]]; //id, direction, x, y
+    brodcasttootherplayers(client, "playermove", res);
+}
+function disconnect(client, data) {
+    if(debug){console.log('disconnect ',client.username);}
+    // the "original" client no longer is in any rooms
+    // we replace him with a copy which is a fake to only get previous room
+    let fakeclient = new Object;
+    fakeclient.rooms = [client.id,client.currentroom]        
+    brodcasttootherplayers(fakeclient, "newjoin");
+}
+function positionFood(client, data) {
+    brodcasttootherplayers(client, "positionFood");
+}
 server.listen(port);
 console.log('server started at',port);
-
 function getrooms(self) { // returns an array of rooms
     return Array.from(self.rooms)
 }
@@ -107,25 +124,11 @@ function getsocketnames(listofids) {
     //     ...
     // ]
 }
-
 function brodcasttootherplayers(client, event, data) {
     let room = getrooms(client)[1]; // get custom room of client
     io.to(room).emit(event,data); // the client when dc, is replaced with a fakeclient which only has room property
     //this is why we need server to send the message.
 }
-function updateroomclients(client, data) {
-    let res = client.rooms;
-    let rooms = getrooms(client);//get the custom room, not id room
-    if(rooms.length < 2) {
-        client.emit("roomclients", []);
-        return true
-    }
-    let room = rooms[1]; //second one is the custom one joined
-    let clientidinroom = currentroomclients(room);
-    let clientnames = getsocketnames(clientidinroom); // this array includes ids
-    client.emit("roomclients", clientnames); // sends array of clients
-}
-
 // data = [
 //     ['socketname','socketid'],
 //     ['socketname','socketid']
