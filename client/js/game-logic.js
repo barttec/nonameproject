a0bac4ab0f731c8ee12e0e8cc600e82c7411baef
bartk <bart.koszek@hotmail.com>
2021-04-20 01:20:35 +0200

    EVERYTHING WORKS!!!!!!!!!!!

--- client/js/game-logic.js
+++ client/js/game-logic.js
@@ -12,10 +12,21 @@ var player1 = {
     },
     decelaration: 0.97,
     speed: 1,
-    moving: false
+    keyarray: [false,false,false,false]// w a s d
 }
-// w a s d
-var keyarray = [false,false,false,false];
+
+// The function gets called when the window is fully loaded
+// Get the canvas and context
+var canvas = document.getElementById("viewport"); 
+var context = canvas.getContext("2d");
+ 
+// Timing and frames per second
+var lastframe = 0;
+var fpstime = 0;
+var framecount = 0;
+var fps = 0;
+ 
+var playerarray = [player1];
 
 function addplayer(color, x, y, name) {
     if(color == undefined) {
@@ -33,11 +44,19 @@ function addplayer(color, x, y, name) {
         x: x,
         y: y,
         size: 16,
-        name: name
+        name: name,
+        velocity: {
+            x: 0,
+            y: 0,
+            dx: 0,
+            dy: 0
+        },
+        decelaration: 0.97,
+        speed: 1,
+        keyarray: [false,false,false,false]// w a s d
     }
     playerarray.push(playerobject);
 }
-
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
@@ -45,21 +64,7 @@ function getRandomColor() {
       color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-  }
-
-// The function gets called when the window is fully loaded
-// Get the canvas and context
-var canvas = document.getElementById("viewport"); 
-var context = canvas.getContext("2d");
- 
-// Timing and frames per second
-var lastframe = 0;
-var fpstime = 0;
-var framecount = 0;
-var fps = 0;
- 
-var playerarray = [player1];
-
+}
 // Initialize the game
 function init() {
     // Add mouse events
@@ -72,7 +77,6 @@ function init() {
     // Enter main loop
     main(0);
 }
- 
 // Main loop
 function main(tframe) {
     // Request animation frames
@@ -82,7 +86,6 @@ function main(tframe) {
     update(tframe);
     render();
 }
- 
 // Update the game state
 function update(tframe) {
     var dt = (tframe - lastframe) / 1000;
@@ -91,7 +94,6 @@ function update(tframe) {
     // Update the fps counter
     updateFps(dt);
 }
- 
 function updateFps(dt) {    
     if (fpstime > 0.25) {
         // Calculate fps
@@ -106,7 +108,7 @@ function updateFps(dt) {
     fpstime += dt;
     framecount++;
 }
- // Get the mouse position
+// Get the mouse position
 function getMousePos(canvas, e) {
     var rect = canvas.getBoundingClientRect();
     return {
@@ -114,13 +116,11 @@ function getMousePos(canvas, e) {
         y: Math.round((e.clientY - rect.top)/(rect.bottom - rect.top)*canvas.height)
     };
 }
-
 // Render the game
 function render() {
     // Draw the frame
     drawFrame();
 }
- 
 // Draw a frame with a border
 function drawFrame() {
     // Draw background and a border
@@ -146,12 +146,14 @@ function drawFrame() {
         player.velocity.x = Math.round((player.velocity.x * player.decelaration)*1000)/1000;
         player.velocity.y = Math.round((player.velocity.y * player.decelaration)*1000)/1000;
 
-        movePlayer();
-        // if(player.moving && (fps % 10 == 0) ) {
-        //     // console.log(player.velocity.dx);
-        //     player.velocity.x = player.velocity.x + player.velocity.dx;
-        //     player.velocity.y = player.velocity.y + player.velocity.dy;
-        // }
+        // decelaration when player is not moving
+        if(player.moving == false) {
+            player.velocity.x = Math.round((player.velocity.x * (player.decelaration*player.decelaration))*1000)/1000;
+            player.velocity.y = Math.round((player.velocity.y * (player.decelaration*player.decelaration))*1000)/1000;    
+        }
+
+        //makes the movement presistent
+        movePlayer(player);
 
         //make the game loop around
         if(player.x > canvas.width){
@@ -167,12 +169,9 @@ function drawFrame() {
             player.y = canvas.height;
         }
     });
-
-    drawText("#000000", "12px Verdana", player1.moving,75, 15)
     //draw fps
     drawFps();      
 }
-
 function drawRectangle(color, x1, y1, dx, dy) {       
     context.fillStyle = color;
     context.fillRect(x1, y1, dx, dy);
@@ -183,7 +182,6 @@ function drawFps() {
     context.font = "12px Verdana";
     context.fillText("Fps: " + fps, 13, 15);
 }
- 
 function drawText(color, font, text, x, y) {
     context.fillStyle = color;
     context.font = font;
@@ -202,29 +200,42 @@ function onMouseOut(e) {}
 function keydown(e) {  
     let press = direction(e);
     updateKeyArray(e, press, true);
-    movePlayer()
+    movePlayer(player1)
+    player1.moving = true;
 }
 function updateKeyArray(e, press, value) {
+    let oldkeyarray = JSON.stringify(player1.keyarray); // deepcopy of player1.keyarray
     switch (press) {
         case "up":
-            keyarray[0] = value;
+            player1.keyarray[0] = value;
             break;
         case "left":
-            keyarray[1] = value;
+            player1.keyarray[1] = value;
             break;
         case "down":
-            keyarray[2] = value;
+            player1.keyarray[2] = value;
             break;
         case "right":
-            keyarray[3] = value;
+            player1.keyarray[3] = value;
             break;
         default:
             break;
+    }    
+    if(oldkeyarray === JSON.stringify(player1.keyarray)){
+        return true;
+    } else {
+        multiplayermove();
     }
+}
+let lastsend = new Date().getTime();
+function multiplayermove() {
+    console.log('server get fucked', new Date().getTime()-lastsend);
+    lastsend = new Date().getTime();
 }
 function keyup(e) {
     let press = direction(e);
     updateKeyArray(e, press, false);
+    player1.moving = false;
 }
 function direction(e) {
     let press;
@@ -242,23 +253,22 @@ function direction(e) {
     }
     return press;
 }
-
-function movePlayer(){
-    for (let index = 0; index < keyarray.length; index++) {        
-        const value = keyarray[index];
+function movePlayer(player){ 
+    for (let index = 0; index < player.keyarray.length; index++) {        
+        const value = player.keyarray[index];
         if(value){
             switch (index) {
                 case 0: // up
-                    player1.velocity.y += -player1.speed;
+                    player.velocity.y += -player.speed;
                     break;
                 case 1: // left
-                    player1.velocity.x += -player1.speed;
+                    player.velocity.x += -player.speed;
                     break;
                 case 2: // down
-                    player1.velocity.y += player1.speed;
+                    player.velocity.y += player.speed;
                     break;
                 case 3: // right
-                    player1.velocity.x += player1.speed;
+                    player.velocity.x += player.speed;
                     break;
                 default:
                     break;
@@ -267,85 +277,5 @@ function movePlayer(){
     }
 
 }
-// function complexMovementHandler(e) {
-//     switch (e.key) {
-//         case "w":
-//             movePlayer(0, -player1.speed, "up");
-//             break;
-//         case "ArrowUp":
-//             movePlayer(0, -player1.speed, "up");
-//             break;
-//         case "s":
-//             movePlayer(0, player1.speed, "down");
-//             break;
-//         case "ArrowDown":
-//             movePlayer(0, player1.speed, "down");
-//             break;
-//         case "a":
-//             movePlayer(-player1.speed, 0, "left");
-//             break;
-//         case "ArrowLeft":
-//             movePlayer(-player1.speed, 0, "left");
-//             break;
-//         case "d":
-//             movePlayer(player1.speed, 0, "right");
-//             break;
-//         case "ArrowRight":
-//             movePlayer(player1.speed, 0, "right");
-//             break;
-//         default:
-//             break;
-//     }
-// }
-//     player1.lastpress = press;
-// function movePlayer(x,y,key) {
-//         // insta stop
-    // if(player1.lastpress != key) {
-    //     switch (key) {
-    //         case "up":
-    //             player1.velocity.y = 0;
-    //             break;
-    //         case "down":
-    //             player1.velocity.y = 0;
-    //             break;
-    //         case "right":
-    //             player1.velocity.x = 0;
-    //             break;
-    //         case "left":
-    //             player1.velocity.x = 0;
-    //             break;
-    //         default:
-    //             break;
-    //     }
-//     }
-//     player1.lastpress = key;
-//     player1.velocity.x = player1.velocity.x + x;
-//     player1.velocity.y = player1.velocity.y + y;
-// }
- // if(player1.lastpress != press) {
-            //     switch (press) {
-            //         case "up":
-            //             player1.velocity.y = 0;
-            //             break;
-            //         case "down":
-            //             player1.velocity.y = 0;
-            //             break;
-            //         case "right":
-            //             player1.velocity.x = 0;
-            //             break;
-            //         case "left":
-            //             player1.velocity.x = 0;
-            //             break;
-            //         default:
-            //             break;
-            //     }
-            // }
 
-// Call init to start the game
 window.onload = init;
-// let x;
-setInterval(() => {
-    console.log(player1.velocity.x,player1.velocity.y);
-}, 100);
-// document.addEventListener('keydown', function(e){console.log(e.timeStamp); x = e.timeStamp});
-// document.addEventListener('keyup', function(e){console.log(x - e.timeStamp);});
