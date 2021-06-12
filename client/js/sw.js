6f9c9539916409a9eceb761c0e91147f89fe6efc
bartk <bart.koszek@hotmail.com>
2021-06-12 10:16:58 +0200

    idk man we pakcing up shop till we figure sum shit out

--- client/js/sw.js
+++ client/js/sw.js
@@ -1,13 +1,13 @@
-function step(tframe) {
-  if(tframe === undefined) {
-    tframe = 1;
-  } else {
-    tframe++;
-  }
-  console.log('work');
-  client.postMessage({
-    msg: "WORK",
-  });
-  setTimeout(function(){step(tframe)},200);
-}
-step()
+// function step(tframe) {
+//   if(tframe === undefined) {
+//     tframe = 1;
+//   } else {
+//     tframe++;
+//   }
+//   console.log('work');
+//   client.postMessage({
+//     msg: "WORK",
+//   });
+//   setTimeout(function(){step(tframe)},200);
+// }
+// step()
